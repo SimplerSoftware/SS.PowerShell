1a8d9cf7ac1445ba74b5d41f41aa72b982d6665b
John Carew <johnwcarew@gmail.com>
2018-03-06 16:30:44 -0600

    Fixed relative path issue.

--- SS.PowerShell/Properties/AssemblyInfo.cs
+++ SS.PowerShell/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -33,7 +33,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1803.0")]
+[assembly: AssemblyFileVersion("1.0.1803.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
+
+[assembly: AssemblyInformationalVersion("1.0.1")]
