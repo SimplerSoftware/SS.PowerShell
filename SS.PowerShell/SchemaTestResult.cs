3f23dc83d01ec3f0d57f0ce0955218a9cb5e61a0
John <johnwc@users.noreply.github.com>
2018-03-06 09:54:13 -0600

    Add project files.

---
+++ SS.PowerShell/SchemaTestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Schema;
+
+namespace SS.PowerShell
+{
+    /// <summary>
+    /// <para type="description">Result object that is returned from schema test.</para>
+    /// </summary>
+    public class SchemaTestResult
+    {
+        public bool Valid { get; internal set; }
+        public IList<ValidationError> Errors { get; internal set; }
+    }
+}
