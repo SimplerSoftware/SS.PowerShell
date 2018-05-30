7f3d6597a16ec670f48bebd6e0853b347911220c
John Carew <johnwcarew@gmail.com>
2018-05-30 12:08:23 -0500

    Updated MsgKit for embedded image bug

--- SS.PowerShell/Properties/AssemblyInfo.cs
+++ SS.PowerShell/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1805.3")]
-[assembly: AssemblyFileVersion("1.0.1805.3")]
+[assembly: AssemblyVersion("1.0.1805.5")]
+[assembly: AssemblyFileVersion("1.0.1805.5")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 
-[assembly: AssemblyInformationalVersion("1.0.27")]
+[assembly: AssemblyInformationalVersion("1.0.29")]
